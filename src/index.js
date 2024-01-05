bad49b435f68f99d02efdfa397cb2d18553c86f0
Ranjan Kumar <ranjanj@edgecorporation.net>
2024-01-05 15:07:16 +0530

    database connect

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv";
 import connectDB from './db/index.js';
+import express from "express";
 
 dotenv.config({
     path: './env'
 })
 
+const PORT = process.env.PORT || 8000;
 connectDB()
+    .then(() => {
+        app.on('err', (err) => {
+            console.log(`Error while connecting to server`);
+            throw err
+        })
+        app.listen(PORT, () => {
+            console.log(`Application running on port ${PORT}`)
+        });
+    })
+    .catch((err) => {
+        console.log('MONGODB Connection Failed!');
+    })
